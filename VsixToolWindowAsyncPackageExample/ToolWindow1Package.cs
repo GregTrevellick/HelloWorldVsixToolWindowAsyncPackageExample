4e3fc25169ba7678febbdcf1c339387d262eddfc
Greg Trevellick <gtrevellick@googlemail.com>
2018-03-16 08:09:37 +0000

    .

--- VsixToolWindowAsyncPackageExample/ToolWindow1Package.cs
+++ VsixToolWindowAsyncPackageExample/ToolWindow1Package.cs
@@ -15,7 +15,6 @@ namespace VsixToolWindowAsyncPackageExample
     [PackageRegistration(UseManagedResourcesOnly = true)]//, AllowsBackgroundLoading = true)]
     [ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]//UIContextGuids.NoSolution vs VSConstants.UICONTEXT.NoSolution_string
     [ProvideMenuResource("Menus.ctmenu", 1)]//[ProvideMenuResource(1000, 1)]
-    //[ProvideOptionPage(typeof(GeneralOptions), Vsix.Name, CommonConstants.CategorySubLevelFootball, 0, 0, true)]
     [ProvideToolWindow(typeof(VsixToolWindowPane2), Style = VsDockStyle.Tabbed, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110057")]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     #endregion
@@ -25,42 +24,7 @@ namespace VsixToolWindowAsyncPackageExample
 
         public ToolWindow1Package()
         {
-            //if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
-            //{
-            //    ChaseRating();
-            //}
         }
-
-        //this block moved to InitializeToolWindowAsync() where potentially expensive work, preferably done on a background thread where possible.
-        //protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        //{
-        //    await ToolWindow1Command.InitializeGregt(this);
-        //    VsixToolWindowPane.GetOptionsFromStoreAndMapToInternalFormatMethod =
-        //        any
-        //            =>
-        //        {
-        //            var generalOptions = (GeneralOptions)GetDialogPage(typeof(GeneralOptions));
-        //            ToolWindow1Control.LeagueGeneralOptions = GetLeagueGeneralOptions(generalOptions);
-        //        };
-        //    VsixToolWindowPane.UpdateLastUpdatedDate =
-        //        any
-        //            =>
-        //        {
-        //            var hiddenOptions = (HiddenOptions)GetDialogPage(typeof(HiddenOptions));
-        //            hiddenOptions.LastUpdated = DateTime.Now;
-        //            hiddenOptions.SaveSettingsToStorage();
-        //        };
-        //    VsixToolWindowPane.GetLastUpdatedDate =
-        //        any
-        //            =>
-        //        {
-        //            var hiddenOptions = (HiddenOptions)GetDialogPage(typeof(HiddenOptions));
-        //            return hiddenOptions.LastUpdated;
-        //        };
-        //}
-
-
-        //https://github.com/Microsoft/VSSDK-Analyzers/blob/master/doc/VSSDK003.md
 
         public override IVsAsyncToolWindowFactory GetAsyncToolWindowFactory(Guid toolWindowType)
         {
@@ -79,31 +43,6 @@ namespace VsixToolWindowAsyncPackageExample
             //await Task.Delay(5000, cancellationToken);
             await VsixToolWindowCommand.InitializeGregt(this);
 
-            #region define actions/funcs for later on
-            //VsixToolWindowPane.GetOptionsFromStoreAndMapToInternalFormatMethod =
-            //    any
-            //        =>
-            //    {
-            //        var generalOptions = (GeneralOptions)GetDialogPage(typeof(GeneralOptions));
-            //        ToolWindow1Control.LeagueGeneralOptions = GetLeagueGeneralOptions(generalOptions);
-            //    };
-            //VsixToolWindowPane.UpdateLastUpdatedDate =
-            //    any
-            //        =>
-            //    {
-            //        var hiddenOptions = (HiddenOptions)GetDialogPage(typeof(HiddenOptions));
-            //        hiddenOptions.LastUpdated = DateTime.Now;
-            //        hiddenOptions.SaveSettingsToStorage();
-            //    };
-            //VsixToolWindowPane.GetLastUpdatedDate =
-            //    any
-            //        =>
-            //    {
-            //        var hiddenOptions = (HiddenOptions)GetDialogPage(typeof(HiddenOptions));
-            //        return hiddenOptions.LastUpdated;
-            //    };
-            #endregion
-
             return "foo"; // this is passed to the tool window constructor
         }
 
@@ -115,38 +54,5 @@ namespace VsixToolWindowAsyncPackageExample
             }
             return base.GetToolWindowTitle(toolWindowType, id);
         }
-
-        //private LeagueGeneralOptions GetLeagueGeneralOptions(GeneralOptions generalOptions)
-        //{
-        //    return new LeagueGeneralOptions
-        //    {
-        //        LeagueOptions = new List<LeagueOption>
-        //        {
-        //            //This is the sequence leagues appear in the UI
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInUk1, InternalLeagueCode.UK1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInUk2, InternalLeagueCode.UK2),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInUk3, InternalLeagueCode.UK3),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInUk4, InternalLeagueCode.UK4),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInEs1, InternalLeagueCode.ES1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInNl1, InternalLeagueCode.NL1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInDe1, InternalLeagueCode.DE1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInDe2, InternalLeagueCode.DE2),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInFr1, InternalLeagueCode.FR1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInFr2, InternalLeagueCode.FR2),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInIt1, InternalLeagueCode.IT1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInIt2, InternalLeagueCode.IT2),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInPt1, InternalLeagueCode.PT1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInUefa1, InternalLeagueCode.UEFA1),
-        //            WpfHelper.GetLeagueOption(generalOptions.InterestedInBr1, InternalLeagueCode.BR1),
-        //        }
-        //    };
-        //}
-
-        //private void ChaseRating()
-        //{
-        //    var hiddenChaserOptions = (IRatingDetailsDto)GetDialogPage(typeof(HiddenRatingDetailsDto));
-        //    var packageRatingChaser = new PackageRatingChaser();
-        //    packageRatingChaser.Hunt(hiddenChaserOptions);
-        //}
     }
 }
--- VsixToolWindowAsyncPackageExample/VsixToolWindowPane2.cs
+++ VsixToolWindowAsyncPackageExample/VsixToolWindowPane2.cs
@@ -8,29 +8,9 @@ namespace VsixToolWindowAsyncPackageExample
     // In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane, usually implemented by the package implementer.
     // This class derives from the ToolWindowPane class provided from the MPF in order to use its implementation of the IVsUIElementPane interface.
 
-    //////////////////////////public ToolWindow1() : base(null)
-    //////////////////////////{
-    //////////////////////////    this.Caption = "ToolWindow1";
-    //////////////////////////    this.Content = new ToolWindow1Control();
-    //////////////////////////}
-
     [Guid("a7e33b03-7489-478d-bb22-57130d19ca29")]
     public class VsixToolWindowPane2 : ToolWindowPane
     {
-        //public static Func<string, DateTime> GetLastUpdatedDate { get; set; }
-        //public static Action<string> GetOptionsFromStoreAndMapToInternalFormatMethod { get; set; }
-        //public static Action<string> UpdateLastUpdatedDate { get; set; }
-
-        //private void SetBossModeCaption()
-        //{
-        //    Caption = "F-crash_.Data";
-        //}
-
-        //private void SetLeagueModeCaption()
-        //{
-        //    Caption = Vsix.Name;
-        //}
-
         public VsixToolWindowPane2(string message) : this()
         {
             //just before this breakpoint the IDE kicks into life 
@@ -39,7 +19,7 @@ namespace VsixToolWindowAsyncPackageExample
 
         public VsixToolWindowPane2() : base(null)
         {
-            this.Caption = "VsixToolWindowPane2 VsixToolWindowAsyncPackageExample gregt1";
+            this.Caption = "VsixToolWindowAsyncPackageExample_Caption";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
